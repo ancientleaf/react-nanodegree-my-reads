d2a3d46bbc62f875b5a052d411f220d76471cf65
Jging Fai Chong <jgingfai.chong@rmit.edu.au>
2021-07-29 23:20:54 +1000

    style: Added missing props type in ListBookShelf

--- src/components/ListBookShelf.js
+++ src/components/ListBookShelf.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BookShelf from './BookShelf';
 import {SHELVES_CONFIG} from '../config/ShelvesConfig';
+import PropTypes from 'prop-types';
 
 class ListBookShelf extends Component {
 
@@ -9,8 +10,13 @@ class ListBookShelf extends Component {
       SHELVES_CONFIG.shelves.map( shelf => (
         <BookShelf key={shelf.id} books={this.props.books.filter(book => book.shelf === shelf.id)} shelf={shelf} moveBookHandler={this.props.moveBookHandler} />
       ))
-    )
+    );
   }
 }
+
+ListBookShelf.propsType = {
+  books: PropTypes.array.isRequired,
+  moveBookHandler: PropTypes.func.isRequired
+};
 
 export default ListBookShelf;
